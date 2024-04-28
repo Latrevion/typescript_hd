bc1d3f2fad67e972c95257671cbf9999b2e50eef
luzhihong <xiaopikaqiu@outlook.com>
2024-04-28 18:32:21 +0800

    23ts

---
+++ 23.js
@@ -0,0 +1,7 @@
+// let a:string
+// a='luke'
+var addUser = function (user) {
+    console.log('add the user');
+    return true;
+};
+addUser({ name: 'luke', age: 18 });
--- 23.ts
+++ 23.ts
@@ -1,2 +1,33 @@
 // let a:string
 // a='luke'
+
+// let hd1:(a:number,b:number)=>number;
+
+// hd1= (x:number,y:number):number=>{
+//     return x+y
+// };
+
+// console.log(hd1(1,1));
+
+// let hd2: (a: number, b: number) => number;
+
+// hd2 = (x: number, y: number): number => {
+//   return x + y;
+// };
+
+// console.log(hd2(1, 1)); // 应该会输出 2
+
+type userType = {name:string,age:number}
+
+type userAddFUn = (user:userType)=>boolean
+
+
+let addUser:userAddFUn=(user:userType):boolean=>{
+    console.log('add the user')
+
+    return true
+}
+
+addUser({name:'luke',age:18})
+
+
