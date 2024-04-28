1c4b07111b540907dc561d0a2c98483ca95e2048
luzhihong <xiaopikaqiu@outlook.com>
2024-04-28 18:54:02 +0800

    25ts,tuple

---
+++ 25.js
@@ -0,0 +1,5 @@
+// let arr:(string|number|boolean)[]=['luke',2030,false]
+// console.log(arr);
+var tuple = ['luke', 12, true];
+tuple[0] = 'looking';
+console.log(tuple);
---
+++ 25.ts
@@ -0,0 +1,8 @@
+// let arr:(string|number|boolean)[]=['luke',2030,false]
+
+// console.log(arr);
+
+
+let tuple:[string,number,boolean]=['luke',12,true]
+tuple[0]='looking';
+console.log(tuple);
