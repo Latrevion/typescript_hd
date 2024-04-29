84be909c25cb8a0af7709599c8f2738c3db24643
luzhihong <xiaopikaqiu@outlook.com>
2024-04-29 09:40:27 +0800

    enum,4.ts

---
+++ enum/4.js
@@ -0,0 +1,6 @@
+var a1 = 'luke';
+var b1 = 123;
+var h1 = [a1, b1];
+var f = h1[1];
+f = 88;
+console.log(f);
---
+++ enum/4.ts
@@ -0,0 +1,8 @@
+let a1 = 'luke'
+let b1 = 123
+
+let h1= <const>[a1,b1]
+
+let f= h1[1]
+f=88
+console.log(f);
