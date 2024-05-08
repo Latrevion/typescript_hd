9090a2fc3758c60a589404fd863ffc0492f3d32f
luzhihong <xiaopikaqiu@outlook.com>
2024-05-08 10:33:21 +0800

    class,13.ts

---
+++ class/13.ts
@@ -0,0 +1,48 @@
+{
+    interface PlayEndInterface{
+        end():void
+    }
+
+
+    interface AnimationInterface extends PlayEndInterface{
+        name: string 
+        move():void
+    }
+
+    abstract class Animation{
+        protected getPos():{x:number,y:number}{
+            return {x:100,y:300}
+        }
+    }
+
+    class Tank extends Animation implements AnimationInterface{
+            name:string = 'tank'
+            public move():void{
+                console.log(`${this.name} move`);
+            }
+                    end(){
+            console.log('game over');
+            
+        }
+    }
+
+    class Player extends Animation implements AnimationInterface{
+        name:string = 'player tank'
+        public move():void{
+            console.log(`${this.name}  move`);
+        }
+        end(){
+            console.log('game over');
+            
+        }
+    }
+
+    const t= new Tank()
+    const player = new Player()
+    t.move()
+    player.move()
+
+}
+
+
+
