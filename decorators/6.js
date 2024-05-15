b9d36aef83887ed6ff25be329130696e178b547f
luzhihong <xiaopikaqiu@outlook.com>
2024-05-15 09:11:48 +0800

    decorators,6.ts

---
+++ decorators/6.js
@@ -0,0 +1,9 @@
+"use strict";
+class LoginController {
+    login() {
+        console.log('loading');
+        console.log('success');
+    }
+}
+const l = new LoginController();
+l.login();
---
+++ decorators/6.ts
@@ -0,0 +1,9 @@
+class LoginController{
+    public login(){
+       console.log('loading');
+       console.log('success');
+    }
+}
+
+const l = new LoginController()
+l.login()
