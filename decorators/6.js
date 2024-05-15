af511ab7741ee2d442a6c2528105c383f4d21520
luzhihong <xiaopikaqiu@outlook.com>
2024-05-15 09:39:59 +0800

    decorators,use ts for Unified messaging

--- decorators/6.js
+++ decorators/6.js
@@ -1,9 +1,33 @@
 "use strict";
-class LoginController {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const MessageDecorator = (target) => {
+    target.prototype.message = (msg) => {
+        console.log(msg);
+    };
+};
+let LoginController = class LoginController {
     login() {
         console.log('loading');
-        console.log('success');
+        this.message('successful');
     }
-}
+};
+LoginController = __decorate([
+    MessageDecorator
+], LoginController);
+let ArticleController = class ArticleController {
+    store() {
+        this.message('add article success');
+    }
+};
+ArticleController = __decorate([
+    MessageDecorator
+], ArticleController);
 const l = new LoginController();
 l.login();
+const a = new ArticleController();
+a.store();
--- decorators/6.ts
+++ decorators/6.ts
@@ -1,9 +1,25 @@
+const MessageDecorator:ClassDecorator= (target:Function){
+    target.prototype.message = (msg:string)=>{
+        console.log(msg);
+    }
+}
+
+@MessageDecorator
 class LoginController{
     public login(){
        console.log('loading');
-       console.log('success');
+       this.message('successful')
+    }
+}
+
+@MessageDecorator
+class ArticleController{
+    public store(){
+        this.message('add article success')
     }
 }
 
 const l = new LoginController()
 l.login()
+const a = new ArticleController()
+a.store()
