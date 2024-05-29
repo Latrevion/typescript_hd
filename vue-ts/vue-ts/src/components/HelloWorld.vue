c96302e42857b717784b3d1419c44ead03eabfac
luzhihong <xiaopikaqiu@outlook.com>
2024-05-29 09:46:56 +0800

    vue-ts

---
+++ vue-ts/vue-ts/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const data =await fetch('http://127.0.0.1:3002/posts/').then(r=>r.json())
+const post = ref(data)
+</script>
+
+<template>
+    {{ post }}
+</template>
+
